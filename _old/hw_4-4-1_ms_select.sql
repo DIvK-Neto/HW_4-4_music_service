e9c68f693261e5aae086c26df3ae1096c7ebf5f1
DIvK <dikryuchenko@yandex.ru>
2024-11-12 23:16:10 +0300

    reworking

--- _old/hw_4-4-1_ms_select.sql
+++ _old/hw_4-4-1_ms_select.sql
@@ -16,17 +16,12 @@ SELECT name  from  artist
 WHERE name NOT LIKE '% %';
 
 -- 2.5 выведем название треков, которые содержат слово «мой» или «my».
-SELECT distinct (title)  from  track
+SELECT title  from  track
 WHERE 
-	title LIKE 'Мой %' or
-	title LIKE '% мой' or
-	title LIKE '% мой %' or
-	title LIKE 'мой' or
-	title LIKE 'My %' or 
-	title LIKE '% my' or
-	title LIKE '% my %' or
-	title LIKE 'my' or
-	title LIKE 'MY'
+	title LIKE '%Мой%' or
+	title LIKE '%мой%' or 
+	title LIKE '%My%' or 
+	title LIKE '%my%'
 ;
 
 
@@ -47,9 +42,10 @@ GROUP BY g.id
 ORDER BY g.id;
 
 -- 3.2 выведем количество треков, вошедших в альбомы 2019–2020 годов.
-SELECT count(t.id) FROM album a
+SELECT a.title, a.year, count(t.id) FROM album a
 JOIN track t ON a.id = t.album_id
-WHERE a.year between 2019 and 2020
+WHERE a.year >= 2019 AND a.year <= 2020
+GROUP BY a.title, a.year
 
 -- 3.3 выведем среднюю продолжительность треков по каждому альбому.
 SELECT a.title, ROUND(AVG(t.duration)) FROM album a
@@ -57,13 +53,10 @@ JOIN track t ON a.id = t.album_id
 GROUP BY a.title 
 
 -- 3.4 выведем всех исполнителей, которые не выпустили альбомы в 2020 году.
-SELECT ar.name FROM artist ar
-WHERE ar.name NOT in (
-	SELECT ar.name FROM artist ar
-	JOIN artistalbum aa ON ar.id = aa.artist_id
-	JOIN album al ON aa.album_id = al.id
-	WHERE al.year = 2020
-);
+SELECT ar.name, al.year FROM artist ar
+JOIN artistalbum aa ON ar.id = aa.artist_id
+JOIN album al ON aa.album_id = al.id
+WHERE al.year != 2020
 
 -- 3.5 выведем названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
 SELECT DISTINCT c.title, a.year FROM collection c
@@ -77,13 +70,14 @@ WHERE ar.name LIKE 'Кино'
 
 -- Задание 4 (дополнительное)
 -- 4.1 выведем названия альбомов, в которых присутствуют исполнители более чем одного жанра.
-select DISTINCT a.title FROM album a
+SELECT a.title FROM album a 
 JOIN artistalbum aa ON a.id = aa.album_id 
 JOIN artist ar ON aa.artist_id = ar.id 
 JOIN artistgenre ag ON ar.id = ag.artist_id 
-JOIN genre gr ON ag.genre_id = gr.id
-group by a.title, ar.id 
-having count(gr.title)>1;
+JOIN genre gr ON ag.genre_id = gr.id 
+GROUP BY a.title 
+HAVING COUNT(DISTINCT gr.id) > 1;
+ 
   
 -- 4.2 выведем наименования треков, которые не входят в сборники.
 SELECT t.title FROM track t
